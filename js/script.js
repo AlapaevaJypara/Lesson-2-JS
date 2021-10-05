cb80b17763882cb3f82f15e0719c489faa8011bd
Jypara Alapaeva <jypara.alapaeva.kk@gmail.com>
2021-10-05 20:54:02 +0600

    my first Js files

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*let userName;
+userName="Жыпара";
+document.write(userName + <br>);
+
+let adress;
+adress="г.Бишкек,ул.Тыныстанова 105";
+document.write(adress);
+
+let number1=200;
+let number2=2021;
+let result=number1+number2;
+document.write(result);
+prompt("Как тебя зовут?", "Иван Иванов");
+let userName= prompt("Как тебя зовут?", "Иван Иванов");
+document.write("Hello"+userName);
+
+prompt("Твой возраст?",45);
+let userAge= prompt("Твой возраст?",45);
+document.write("userAge");
+
+prompt("Твой город?" ,"New Bishkek");
+let userCity= prompt("Твой город?" ,"New Bishkek");
+document.write("userCity");
+
+let userName = prompt("Привет, как тебя зовут?", "Иван");
+document.write("Привет! " + userName);*/
+let userName = prompt("Ваше имя?");
+let age = prompt("Ваш возраст?");
+
+document.write("<h4>Здравствуйте " + userName + ", добро пожаловать!" + " Вам " + age + " лет, " + "Приятно с Вами познакомиться!😊" + "</h4>");
+
